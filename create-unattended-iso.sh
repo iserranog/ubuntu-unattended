c6cdb1fe38d1c807f7b45f5dcbb5f0230afb06a1
iserranog <iserranog@gmail.com>
2018-08-22 17:31:37 +0200

    Update create-unattended-iso.sh

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -223,7 +223,7 @@ touch $tmp/iso_new/preseed/custom/hello.txt
 # include firstrun script
 echo "
 # setup firstrun script
-d-i preseed/late_command string touch mm.txt;  " >> $tmp/iso_new/preseed/$seed_file
+d-i preseed/late_command string in-target touch mm.txt; cp $tmp/iso_new/preseed/custom/hello.txt .  " >> $tmp/iso_new/preseed/$seed_file
 
 # generate the password hash
 pwhash=$(echo $password | mkpasswd -s -m sha-512)
